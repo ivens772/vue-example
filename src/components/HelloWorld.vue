837a7b662fdb29a040711b069bd928fbb6f879cb
ivens772 <iacalderon@innovacion.gob.sv>
2024-06-04 10:31:02 -0600

    third  commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,10 +2,11 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
+     ¿Quieres Casarte con migo?
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
+     <input type="text" v-model="answer" placeholder="responde aqui">
+     <button @click="respuesta">Respuesta</button>
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
@@ -32,7 +33,7 @@
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -40,6 +41,20 @@ export default defineComponent({
   props: {
     msg: String,
   },
+  data(){
+    return {
+      answer :''
+    };
+  },
+  methods:{
+    respuesta:()=>{
+      if (this.answer === "si") {
+        alert(`ud ha colocado que ${this.answer}: felicidades`)
+      }else{
+        alert("vayase a dormir")
+      }
+    }
+  }
 });
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Te amo Yesenia Isamar"/>
   </div>
 </template>
 
-<script lang="ts">
+<script >
 import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
